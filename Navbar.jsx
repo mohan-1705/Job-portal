6ffa200a6a1b85df6b24d1bab941912dece3577e
mohan-1705 <164631881+mohan-1705@users.noreply.github.com>
2024-10-16 21:39:59 +0530

    Update Navbar.jsx

--- Navbar.jsx
+++ Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Context } from "../../main";
 import { Link, useNavigate } from "react-router-dom";
-import axios from "axios";
+import axios from "axios"; 
 import toast from "react-hot-toast";
 import { GiHamburgerMenu } from "react-icons/gi";
 
