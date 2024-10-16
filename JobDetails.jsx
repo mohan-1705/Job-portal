0a3cc448ba19ab0e597410801523185d37e78d08
mohan-1705 <164631881+mohan-1705@users.noreply.github.com>
2024-10-16 21:38:15 +0530

    Update JobDetails.jsx

--- JobDetails.jsx
+++ JobDetails.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom"; 
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { Context } from "../../main";
